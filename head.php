0fb3a5aa333b70c550f85c99641a96a6cce96101
LianaTa <vvvooo@yandex.ru>
2019-04-04 14:40:39 +0300

    Create head.php

---
+++ head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="robots" content="all"/>
+        <meta name='yandex-verification' content='6df272e76b15f2cb' >
+        <link rel="icon" href="/image/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="/gallery/css/bootstrap-image-gallery.css">
+        <link href = "/bootstrap/css/bootstrap.css" rel ="stylesheet">
+        <link href = "/bootstrap/css/bootstrap-responsive.css" rel = "stylesheet">
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="http://blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+        <link href = "/css/style.css" rel ="stylesheet">
+        <script type = "text/javascript" src = "/bootstrap/js/jquery.js" ></script>
+        <script type = "text/javascript" src = "/bootstrap/js/bootstrap.js" ></script>
+        <script type = "text/javascript" src = "/bootstrap/js/bootstrap-dropdown.js" ></script>
+        <script type = "text/javascript" src = "/js/holder.js" ></script>
+        <script type = "text/javascript" src = "/js/functions.js"></script>
+        <script type = "text/javascript" src = "/admin/js/admin_functions.js"></script>
+        <script type="text/javascript" src="//vk.com/js/api/openapi.js?111"></script>
+        <script type="text/javascript">
+            VK.init({apiId: 4321145, onlyWidgets: true});
+        </script>
+
