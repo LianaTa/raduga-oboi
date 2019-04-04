9a4177163e890a5649d178427443096648bb14ce
LianaTa <vvvooo@yandex.ru>
2019-04-04 14:41:18 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,28 @@
+<div class = "footer container">
+            <div class = "row">
+                <div class = "span4">
+           	        <!--LiveInternet counter--><script type="text/javascript"><!--
+	        	    document.write("<a href='http://www.liveinternet.ru/click' "+
+	  	            "target=_blank><img src='//counter.yadro.ru/hit?t18.6;r"+
+	   	            escape(document.referrer)+((typeof(screen)=="undefined")?"":
+	  	            ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+	  	            screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+	  	            ";"+Math.random()+
+	  	            "' alt='' title='LiveInternet: показано число просмотров за 24"+
+	  	            " часа, посетителей за 24 часа и за сегодня' "+
+	  	            "border='0' width='88' height='31'><\/a>")
+	    	        //--></script><!--/LiveInternet-->
+	    	        <div class = "span3"  id="vk_like"  ></div>
+                    <script type="text/javascript">
+                        VK.Widgets.Like("vk_like", {type: "button", height: 24});
+                    </script>
+	            </div>
+                <div class = "span7 developer">
+                    Разработка сайта: <a href = "mailto: vvvooo@yandex.ru" title = "vvvooo@yandex.ru">vvvooo@yandex.ru </a>&nbsp;&nbsp;
+                    <script type="text/javascript" src="http://download.skype.com/share/skypebuttons/js/skypeCheck.js"></script> 
+                    <a  href="skype:LikusikL?chat"><img src="http://mystatus.skype.com/mediumicon/LikusikL" style="border: none;" width="26" height="26" alt="My status" />Liana</a>
+                </div>
+	       </div>
+        </div>
+    </body>
+</html>
